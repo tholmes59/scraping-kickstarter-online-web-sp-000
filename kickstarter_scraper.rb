d0ae08b636a10a802babd3708f58031fe62e2f5c
Tom Holmes <tholmes59@gmail.com>
2019-02-01 23:31:55 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,10 +11,11 @@ def create_project_hash
   
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
-    image link: project.css("div.project-thumbnail a img").attribute("src").value
-    description: project.css("p.bbcard_blurb").text
-    percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
-    projects[project] = {}
+    projects[title.to_sym] = {
+    :image link => project.css("div.project-thumbnail a img").attribute("src").value,
+    :description => project.css("p.bbcard_blurb").text,
+    :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+    
   end
   
   projects
