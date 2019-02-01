ec80283e7c170b553e8e0d32d278cecf59ccd5d9
Tom Holmes <tholmes59@gmail.com>
2019-02-01 23:33:53 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,6 +14,7 @@ def create_project_hash
     projects[title.to_sym] = {
     :image link => project.css("div.project-thumbnail a img").attribute("src").value,
     :description => project.css("p.bbcard_blurb").text,
+    :location => project.css("ul.project-meta span.location-name").text,
     :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     }
   end
